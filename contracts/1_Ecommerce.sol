e1392497401357ca9bd7b24b478ea27c1e91408b
curious <vishal238128@gmail.com>
2022-11-05 12:21:33 +0530

    changes in ecommerce

--- contracts/1_Ecommerce.sol
+++ contracts/1_Ecommerce.sol
@@ -15,6 +15,9 @@ contract Ecommerce{
     
     uint counter = 1;
     Product[] public products;
+    event registered(string title, uint productId, address seller);
+    event bought(uint productId, address buyer);
+    event deliverd(uint productId);
 
     function registerProduct(string memory _title, string memory _desc, uint _price) public {
         require(_price > 0, "Price should be greater than zero");
@@ -26,5 +29,20 @@ contract Ecommerce{
         tempProduct.productId = counter;
         products.push(tempProduct);
         counter++;
+        emit registered(_title, tempProduct.productId, msg.sender);
+    }
+
+    function buy(uint _productId) payable public{
+        require(products[_productId - 1].price == msg.value, "please pay the exact price");
+        require(products[_productId - 1].seller != msg.sender, "Seller can't ne buyer");
+        products[_productId - 1].buyer = msg.sender;
+        emit bought(_productId, msg.sender);
+    }
+
+    function delivery(uint _productId) public{
+        require(products[_productId - 1].buyer == msg.sender, "Only buyer can confirm it");
+        products[_productId - 1].deliverd = true;
+        products[_productId - 1].seller.transfer(products[_productId - 1].price);
+        emit deliverd(_productId);
     }
 }
